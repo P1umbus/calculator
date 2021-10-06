8cbdf1fc1224471046721a9aded618c7d843193b
Plumbus <1o1n1e1u1p1@gmail.com>
2021-10-06 22:52:35 +0300

    Доработан

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -8,15 +8,21 @@ public class Manager : MonoBehaviour
     [SerializeField] private Text _output;
     [SerializeField] private Text _output2;
 
-    private string _sign;
+    private string _sign; //Хранит в себе математический знак
 
-    private double[] _numbers = new double[2];
-    private int _i;
+    private string _numberStr; //Хранит в себе число в формате строки. Реализует возможность работы калькулятора с многозначными и отрицательными числами
 
-    private string _numberStr;
-    private string _secondSign;
+    private double _newNumber; //Поле куда записываеться новые числа с ввода. Используеться для подсчетов.
+    private double _res; //Хранит в себе резултат математических вычислений.
 
-    private double _numberX;
+    private string _secondSign; //Тоже хранит в себе знак. Используеться в ситуации где разные приоритеты выполнения подсчета.
+                                //Пример 2 + 2 * 2, в данной ситауции эта переменная будет хранить знак "*".
+    private double _numberX; //Дополнительная переменная которая используеться в ситуации где разные приоритеты подсчета.
+
+    private void Start()
+    {
+        Clear();
+    }
 
     public void OnButtonClick()
     {
@@ -28,141 +34,161 @@ public class Manager : MonoBehaviour
             return;
         }
 
-        _output2.text += input;
+        _output2.text += input; //!
 
+        if (float.TryParse(input, out float num)) 
+        {
+            EnteringNum(input); //При вводе числа записываю его в строку, для возможности получения многозначного числа.
+        }
+        else
+        {
+            if (input == ",") //Логика кнопки ",".
+            {
+                EnteringСomma();
+            }
+            else if (input == "-" && _numberStr == null) //Реализация отрицательных чисел).
+            {
+                _numberStr = "-";
+            }
+            else
+            {
+                if (_numberStr == "-")
+                    _numberStr = "-0";
+
+                if (_secondSign != null && _numberStr == null) 
+                {
+                    _secondSign = null;
+                }
+                else if (_secondSign != null)
+                {
+                    PriorityCounting(input);
+                    return;
+                }
+
+                if (_numberStr != null)
+                    _newNumber = double.Parse(_numberStr);
+
+                _numberStr = null;
+
+                if (CheckingPriorities(input) == false)
+                {
+                    _secondSign = input;
+                    return;
+                }
+
+                _res = Calculate(_sign, _res, _newNumber);
+
+                if (input != "sqrt")
+                    _sign = input;
+
+                return;
+            }
+        }
+
+        if (_secondSign != null)
+        {
+            _newNumber = Calculate(_secondSign, _newNumber, _numberX);
+        }
+
+        _output.text = Convert.ToString(Calculate(_sign, _res, _newNumber));
+    }   
+
+    private bool CheckingPriorities (string input) //Проверяет приоритет расчетов
+    {
+        bool fine = true;
+        if ((_sign == "+" || _sign == "-") && (input == "*" || input == "/"))
+            fine = false;
+
+        return fine;
+    }
+
+    private void EnteringNum(string input) //Записывает число в виде строки в переменные
+    {
         if (float.TryParse(input, out float num))
         {
             if (_numberStr != null)
                 _numberStr += input;
             else
                 _numberStr = input;
-        }
-        else
-        {
-            if (input == ",")
-            {
-                if (_numberStr != null)
-                    _numberStr += ',';
-                else
-                    _numberStr = "0,";
-            }
-            else if (input == "-" && _numberStr == null)
-            {
-                _numberStr = "-";
-            }
+
+            if (_secondSign == null)
+                _newNumber = double.Parse(_numberStr);
             else
-            {
-                ParseNumStrToNumbers();
-
-                if (input == "sqrt")
-                {                    
-                    Sqrt();
-                    return;
-                }
-
-                if (_secondSign != null)
-                    CUWLName();
-
-                if ((_sign == "+" || _sign == "-") && (input == "*" || input == "/"))
-                {
-                    _secondSign = input;
-                    return;
-                }
-                else if (_sign != null)
-                {
-                    Calculate();
-                }
-
-                _sign = input;
-            }
-        }
-    }
-
-    private void ParseNumStrToNumbers()
-    {
-        if (_secondSign != null)
-        {
-            _numberX = float.Parse(_numberStr);
+                _numberX = double.Parse(_numberStr);
         }
         else
         {
-            if (_i > 1)
-                _i = 0;
-            _numbers[_i] = float.Parse(_numberStr);
-            _i++;
+            Debug.LogError("Этот метод может принимать только числа в виде строк!");
         }
-
-        _numberStr = null;
     }
 
-    private void Calculate()
+    private void EnteringСomma() //Реализация запятой
     {
-        if (_sign == null)
-            _sign = "+";
+        if (_numberStr != null && _numberStr.Length < 2)
+            _numberStr += ',';
+        else if (_numberStr == null)
+            _numberStr = "0,";
+    }
 
-        switch (_sign)
+    private void PriorityCounting(string input) //Выполняет приоритетный подсчет.
+    {
+        _numberStr = null;
+
+        _numberX = 0;
+
+        if (CheckingPriorities(input) == false)
+        {
+            _secondSign = input;
+        }
+        else
+        {
+            _secondSign = null;
+
+            _res = Calculate(_sign, _res, _newNumber);
+
+            _sign = input;
+        }
+    }
+
+    private double Calculate(string sign, double num1 = 0, double num2 = 0) //Выполняет расчет.
+    {
+        double res = 0;
+
+        if (sign == null)
+            sign = "+";
+
+        switch (sign)
         {
             case "+":
-                _numbers[0] += _numbers[1];
-                _i = 1;
-                _output.text = Convert.ToString(_numbers[0]);
+                res = num1 + num2;
                 break;
             case "-":
-                _numbers[0] -= _numbers[1];
-                _i = 1;
-                _output.text = Convert.ToString(_numbers[0]);
+                res = num1 - num2;
                 break;
             case "*":
-                _numbers[0] *= _numbers[1];
-                _i = 1;
-                _output.text = Convert.ToString(_numbers[0]);
+                res = num1 * num2;
                 break;
             case "/":
-                _numbers[0] /= _numbers[1];
-                _i = 1;
-                _output.text = Convert.ToString(_numbers[0]);
-                break;
-            case "=":
-                _output.text = Convert.ToString(_numbers[0]);
-                _output2.text += _numbers[0];
+                res = num1 / num2;
                 break;
         }
+
+        return res;
     }
 
-    private void Clear()
+    private void Clear() // Сбрасывает все поля.
     {
         _sign = null;
+        _secondSign = null;
 
         _output.text = "0";
         _output2.text = "";
 
-        _i = 0;
+        _newNumber = 0;
+        _res = 0;
 
-        _numbers = new double[2];
-    }
+        _numberX = 0;
 
-    private void CUWLName()
-    {
-        if (_secondSign == "*")
-            _numbers[1] *= _numberX;
-        else if (_secondSign == "/")
-            _numbers[1] /= _numberX;
-
-        _secondSign = null;
-    }
-
-    private void Sqrt()
-    {
-        if (_i > 1)
-            _i = 0;
-        _i++;
-        if (_i > 1)
-            _i = 0;
-
-        _numbers[_i] = Math.Sqrt(_numbers[_i]);
-
-        _i++;
-
-        Calculate();
+        _numberStr = null;
     }
 }
